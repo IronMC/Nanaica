400576318f24e8ceac876a4ce06740b815bd8bc9
PancakseYT <84547061+PancakseYT@users.noreply.github.com>
2023-07-11 20:27:53 -0600

    Create banmods.sh

---
+++ scripts/banmods.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# List of mod ids to ban
+BANNED_MODS=(
+  "examplemod"
+  "anotherbadmod" 
+)
+
+# Loop through connected players
+for player in $(mc status players); do
+
+  # Get list of player's mods
+  mods=$(mc info $player mods)
+
+  # Loop through banned mods
+  for mod in "${BANNED_MODS[@]}"; do
+    
+    # Check if player has banned mod
+    if [[ $mods == *"$mod"* ]]; then
+      
+      # Ban player
+      mc ban $player "Using banned mod: $mod"
+      
+      # Log ban
+      echo "$(date) - Banned $player for using $mod" >> bans.log
+      
+    fi
+    
+  done
+  
+done
